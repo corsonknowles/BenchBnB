e5e4f4e97c367d11c690f09a2aab5913b3755c15
David Corson-Knowles <david.corsonknowles@gmail.com>
2017-07-14 15:38:46 -0700

    Add root page and controller

---
+++ app/controllers/api/sessions_controller_controller.rb
@@ -0,0 +1,9 @@
+class Api::SessionsControllerController < ApplicationController
+  def create
+  end
+
+  def destroy
+  end
+
+  
+end
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,53 @@
-var path = require('path');
+const path = require('path');
+
 module.exports = {
   context: __dirname,
-  entry: "./frontend/index.jsx",
+  entry: './frontend/index.jsx',
   output: {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
-    filename: "bundle.js"
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
   },
   module: {
     loaders: [
       {
-        test: [/.jsx?$/, /.js?$/],
+        test: /.jsx?$/,
         exclude: /(node_modules)/,
         loader: 'babel-loader',
         query: {
-          presets: ['es2015', 'react']
+          presets: ['react', 'es2015']
         }
       }
     ]
   },
-  devtool: 'source-map',
-  resolve: {
-    extensions: [".js", ".jsx", "*"]
-  }
+  devtool: 'source-maps'
 };
+
+
+// var path = require('path');
+// module.exports = {
+//   context: __dirname,
+//   entry: "./frontend/index.jsx",
+//   output: {
+//     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+//     filename: "bundle.js"
+//   },
+//   module: {
+//     loaders: [
+//       {
+//         test: [/.jsx?$/, /.js?$/],
+//         exclude: /(node_modules)/,
+//         loader: 'babel-loader',
+//         query: {
+//           presets: ['es2015', 'react']
+//         }
+//       }
+//     ]
+//   },
+//   devtool: 'source-map',
+//   resolve: {
+//     extensions: [".js", ".jsx", "*"]
+//   }
+// };
